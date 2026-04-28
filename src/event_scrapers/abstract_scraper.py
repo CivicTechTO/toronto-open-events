6f1ebb96ff30f06294d732f9dcb6842878dcd62e
Isaac McAuley <mcauleyisaac@gmail.com>
2026-04-28 19:25:42 -0400

    Cleaned up abstract scraper

--- src/event_scrapers/abstract_scraper.py
+++ src/event_scrapers/abstract_scraper.py
@@ -5,6 +5,8 @@ class AbstractScraper(scrapy.Spider):
     name = ""
     start_urls = []
 
+    additional_data_for_all = {}
+
     def parse(self, response):
         event_htmls = self.get_all_events(response)
         for e in event_htmls:
@@ -38,6 +40,27 @@ class AbstractScraper(scrapy.Spider):
         """
         raise NotImplementedError
 
+    def get_event_description(self, event_html):
+        """
+        @param event_html: html object of a single event
+        @return: string of the event description
+        """
+        return None
+
+    def get_event_location(self, event_html):
+        """
+        @param event_html: html object of a single event
+        @return: string of the event location
+        """
+        return None
+
+    def get_event_addition_data(self, event_html):
+        """
+        @param event_html: html object of a single event
+        @return: string of the event location
+        """
+        return {}
+
     def get_all_events(self, response):
         """
         @param reponse: scrapy response object
@@ -46,11 +69,17 @@ class AbstractScraper(scrapy.Spider):
         raise NotImplementedError
 
     def event_html_to_object(self, event_html):
-        return EventItem(
-            title=self.get_event_title(event_html),
-            link=self.get_event_url(event_html),
-            organizer_link=self.ORGANIZER_LINK,
-            organizer_name=self.ORGANIZER_NAME,
-            start_datetime=self.get_start_date(event_html)
-        )
+        event_attributes = {
+            "title": self.get_event_title(event_html),
+            "link": self.get_event_url(event_html),
+            "organizer_link": self.ORGANIZER_LINK,
+            "organizer_name": self.ORGANIZER_NAME,
+            "start_datetime": self.get_start_date(event_html),
+            "description": self.get_event_description(event_html),
+            "location": self.get_event_location(event_html),
+            "additional_data": {**self.get_event_addition_data(event_html), **self.additional_data_for_all}
+        }
+        event_attributes = {k: v for k, v in event_attributes.items() if v is not None or v is not {} or v is not []}
+
+        return EventItem(**event_attributes)
 
